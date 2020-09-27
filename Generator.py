2a6c9c7fd9eb5ac291cdf07f918292f3a182c2b5
Karina <karyzenk@gmail.com>
2020-09-27 22:03:56 +0300

    4 eddit

--- Generator.py
+++ Generator.py
@@ -1,12 +1,10 @@
-def my_range(start=1):
-    while True:
-        if start % 3 == 0:
-            yield 'Vasilyi'
-        else: 
-            yield start
-        start += 1
-results = list(my_range())
-gen = my_range()
-i = input('Enter number, please: ')
-for i in range(i):
-    print(next(gen))
+def my_range(start, stop):
+    i = start
+    while i < stop:
+        if i % 3 == 0:
+            yield "Vasilyi"
+        else:
+            yield i
+        i += 1
+resutls = list(my_range(1, 7))
+print(f'{resutls}')
